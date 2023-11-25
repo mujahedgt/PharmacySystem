af3a899371d59794396d84602e7247c74bd8bab3
Mujahed Issa <Mujahedissagt@outlook.com>
2023-11-25 12:17:27 +0300

    LogIn changes

--- PharmacySystem/Form1.Designer.cs
+++ PharmacySystem/Form1.Designer.cs
@@ -39,9 +39,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(303, 392);
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(270, 387);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(142, 24);
+            this.button1.Size = new System.Drawing.Size(201, 43);
             this.button1.TabIndex = 0;
             this.button1.Text = "Log In ";
             this.button1.UseVisualStyleBackColor = true;
@@ -49,16 +50,19 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(288, 316);
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(326, 316);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(179, 20);
+            this.textBox1.Size = new System.Drawing.Size(179, 29);
             this.textBox1.TabIndex = 1;
+            this.textBox1.UseSystemPasswordChar = true;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(288, 250);
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(326, 250);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(179, 20);
+            this.textBox2.Size = new System.Drawing.Size(179, 29);
             this.textBox2.TabIndex = 2;
             // 
             // label1
@@ -67,9 +71,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(168, 253);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.Size = new System.Drawing.Size(114, 24);
             this.label1.TabIndex = 3;
             this.label1.Text = "User Name";
             // 
@@ -79,9 +84,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(168, 322);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.Size = new System.Drawing.Size(100, 24);
             this.label2.TabIndex = 4;
             this.label2.Text = "Password";
             // 
