94b93aa65f13d32924db9ac270eaac93db54324d
juken 30 <junaid123456junaidjunaid@gmail.com>
2023-11-30 22:17:29 +0300

    123231

--- PharmacySystem/users_control.Designer.cs
+++ PharmacySystem/users_control.Designer.cs
@@ -32,9 +32,10 @@
             this.ScientificSerch = new System.Windows.Forms.RadioButton();
             this.BrandNameSerch = new System.Windows.Forms.RadioButton();
             this.label1 = new System.Windows.Forms.Label();
-            this.Add_users = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // users_list
@@ -82,19 +83,9 @@
             this.label1.TabIndex = 18;
             this.label1.Text = "----note----: make a popup message if the admin want to change\r\nthe state";
             // 
-            // Add_users
-            // 
-            this.Add_users.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Add_users.FormattingEnabled = true;
-            this.Add_users.ItemHeight = 20;
-            this.Add_users.Location = new System.Drawing.Point(366, 233);
-            this.Add_users.Name = "Add_users";
-            this.Add_users.Size = new System.Drawing.Size(329, 84);
-            this.Add_users.TabIndex = 19;
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(472, 355);
+            this.button1.Location = new System.Drawing.Point(465, 355);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(118, 38);
             this.button1.TabIndex = 20;
@@ -112,14 +103,33 @@
             this.label2.TabIndex = 21;
             this.label2.Text = "----note----: make a popup message if the admin want \r\nadd a user";
             // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(538, 269);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 26);
+            this.textBox1.TabIndex = 22;
+            this.textBox1.Text = "password";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(366, 269);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(156, 26);
+            this.textBox2.TabIndex = 23;
+            this.textBox2.Text = "user name";
+            // 
             // users_control
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(707, 430);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.Add_users);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.ScientificSerch);
             this.Controls.Add(this.BrandNameSerch);
@@ -137,8 +147,9 @@
         private System.Windows.Forms.RadioButton ScientificSerch;
         private System.Windows.Forms.RadioButton BrandNameSerch;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListBox Add_users;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
